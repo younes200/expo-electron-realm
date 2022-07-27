5d1251fe45579f0887ffe2f950b01e03ed12d40e
Younes <younes.benaomar@gmail.com>
2022-07-28 00:42:13 +0100

    babel include realm

--- electron-webpack.js
+++ electron-webpack.js
@@ -1,6 +1,12 @@
 const { withExpoAdapter } = require('@expo/electron-adapter');
+const path = require("path");
 
 module.exports = withExpoAdapter({
   projectRoot: __dirname,
   // Provide any overrides for electron-webpack: https://github.com/electron-userland/electron-webpack/blob/master/docs/en/configuration.md
+  main: {
+    // "extraEntries": ["@/preload.js"],
+    sourceDirectory: path.join(__dirname, "electron/main"),
+    // "webpackConfig": "custom.webpack.additions.js"
+  },
 });
